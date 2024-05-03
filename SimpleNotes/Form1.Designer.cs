7fc2ac0ba26023af31d520c01283e1bc1b14cd9c
Emil <emil.levin125@gmail.com>
2024-05-03 17:21:34 +0400

    Added basic functionality

--- SimpleNotes/Form1.Designer.cs
+++ SimpleNotes/Form1.Designer.cs
@@ -28,32 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.boxTitle = new System.Windows.Forms.TextBox();
+            this.boxNote = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.previousNotes = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.BtnDelete = new System.Windows.Forms.Button();
-            this.BtnSave = new System.Windows.Forms.Button();
-            this.BtnNewNote = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNewNote = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.previousNotes)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // boxTitle
             // 
-            this.textBox1.Location = new System.Drawing.Point(371, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(417, 23);
-            this.textBox1.TabIndex = 0;
+            this.boxTitle.Location = new System.Drawing.Point(371, 32);
+            this.boxTitle.Name = "boxTitle";
+            this.boxTitle.Size = new System.Drawing.Size(417, 23);
+            this.boxTitle.TabIndex = 0;
             // 
-            // textBox2
+            // boxNote
             // 
-            this.textBox2.Location = new System.Drawing.Point(371, 81);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(417, 357);
-            this.textBox2.TabIndex = 1;
+            this.boxNote.Location = new System.Drawing.Point(371, 81);
+            this.boxNote.Multiline = true;
+            this.boxNote.Name = "boxNote";
+            this.boxNote.Size = new System.Drawing.Size(417, 357);
+            this.boxNote.TabIndex = 1;
             // 
             // label1
             // 
@@ -77,6 +77,7 @@
             // 
             // previousNotes
             // 
+            this.previousNotes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.previousNotes.BackgroundColor = System.Drawing.Color.White;
             this.previousNotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.previousNotes.Location = new System.Drawing.Point(12, 12);
@@ -84,54 +85,55 @@
             this.previousNotes.RowTemplate.Height = 25;
             this.previousNotes.Size = new System.Drawing.Size(353, 346);
             this.previousNotes.TabIndex = 4;
+            this.previousNotes.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.previousNotes_CellDoubleClick);
             // 
-            // button1
+            // btnLoad
             // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(12, 364);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 34);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Load";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnLoad.BackColor = System.Drawing.Color.White;
+            this.btnLoad.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLoad.Location = new System.Drawing.Point(12, 364);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(173, 34);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
-            // BtnDelete
+            // btnDelete
             // 
-            this.BtnDelete.BackColor = System.Drawing.Color.White;
-            this.BtnDelete.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.BtnDelete.Location = new System.Drawing.Point(192, 364);
-            this.BtnDelete.Name = "BtnDelete";
-            this.BtnDelete.Size = new System.Drawing.Size(173, 34);
-            this.BtnDelete.TabIndex = 7;
-            this.BtnDelete.Text = "Delete";
-            this.BtnDelete.UseVisualStyleBackColor = false;
-            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            this.btnDelete.BackColor = System.Drawing.Color.White;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.Location = new System.Drawing.Point(192, 364);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(173, 34);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
-            // BtnSave
+            // btnSave
             // 
-            this.BtnSave.BackColor = System.Drawing.Color.White;
-            this.BtnSave.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.BtnSave.Location = new System.Drawing.Point(191, 404);
-            this.BtnSave.Name = "BtnSave";
-            this.BtnSave.Size = new System.Drawing.Size(173, 34);
-            this.BtnSave.TabIndex = 8;
-            this.BtnSave.Text = "Save";
-            this.BtnSave.UseVisualStyleBackColor = false;
-            this.BtnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            this.btnSave.BackColor = System.Drawing.Color.White;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.Location = new System.Drawing.Point(191, 404);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(173, 34);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // BtnNewNote
+            // btnNewNote
             // 
-            this.BtnNewNote.BackColor = System.Drawing.Color.White;
-            this.BtnNewNote.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.BtnNewNote.Location = new System.Drawing.Point(12, 404);
-            this.BtnNewNote.Name = "BtnNewNote";
-            this.BtnNewNote.Size = new System.Drawing.Size(173, 34);
-            this.BtnNewNote.TabIndex = 9;
-            this.BtnNewNote.Text = "New Note";
-            this.BtnNewNote.UseVisualStyleBackColor = false;
-            this.BtnNewNote.Click += new System.EventHandler(this.BtnNewNote_Click);
+            this.btnNewNote.BackColor = System.Drawing.Color.White;
+            this.btnNewNote.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNewNote.Location = new System.Drawing.Point(12, 404);
+            this.btnNewNote.Name = "btnNewNote";
+            this.btnNewNote.Size = new System.Drawing.Size(173, 34);
+            this.btnNewNote.TabIndex = 9;
+            this.btnNewNote.Text = "New Note";
+            this.btnNewNote.UseVisualStyleBackColor = false;
+            this.btnNewNote.Click += new System.EventHandler(this.btnNewNote_Click);
             // 
             // NoteTaker
             // 
@@ -139,15 +141,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.BtnNewNote);
-            this.Controls.Add(this.BtnSave);
-            this.Controls.Add(this.BtnDelete);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnNewNote);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.previousNotes);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.boxNote);
+            this.Controls.Add(this.boxTitle);
             this.Name = "NoteTaker";
             this.Text = "NoteTaker";
             this.Load += new System.EventHandler(this.NoteTaker_Load);
@@ -159,14 +161,14 @@
 
         #endregion
 
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox boxTitle;
+        private TextBox boxNote;
         private Label label1;
         private Label label2;
         private DataGridView previousNotes;
-        private Button button1;
-        private Button BtnDelete;
-        private Button BtnSave;
-        private Button BtnNewNote;
+        private Button btnLoad;
+        private Button btnDelete;
+        private Button btnSave;
+        private Button btnNewNote;
     }
 }
